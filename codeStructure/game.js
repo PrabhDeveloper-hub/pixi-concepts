7147bfee1a569131f73cf7d2e9e759b67ad2476c
Prabhjot <prsingh@grepruby.io>
2026-01-13 15:26:40 +0530

    space setting

--- codeStructure/game.js
+++ codeStructure/game.js
@@ -2,49 +2,49 @@ import * as PIXI from "./pixi.js";
 import {UI} from "./UI.js";
 
 export default class Game {
-  constructor() {
-    document.body.style.margin = "0px";
-    document.body.style.padding = "0px";
-    document.body.style.overflow = "hidden";
+    constructor() {
+        document.body.style.margin = "0px";
+        document.body.style.padding = "0px";
+        document.body.style.overflow = "hidden";
 
-    this.app = new PIXI.Application({
-      background: "#1099bb",
-      resizeTo: window
-    })
+        this.app = new PIXI.Application({
+            background: "#1099bb",
+            resizeTo: window
+        })
 
-    document.body.appendChild(this.app.view);
+        document.body.appendChild(this.app.view);
 
-    this.init();
-  }
+        this.init();
+    }
 
-  async init() {
-    // alert("GAME LOADED");
-    PIXI.Assets.addBundle('level-1', {
-      bunny: "https://pixijs.com/assets/bunny.png"
-    })
+    async init() {
+        // alert("GAME LOADED");
+        PIXI.Assets.addBundle('level-1', {
+            bunny: "https://pixijs.com/assets/bunny.png"
+        })
 
-    const assets = await PIXI.Assets.loadBundle('level-1');
+        const assets = await PIXI.Assets.loadBundle('level-1');
 
-    this.bunny = new PIXI.Sprite(assets.bunny);
-    this.bunny.x = this.app.screen.width / 2;
-    this.bunny.y = this.app.screen.height / 2.5;
-    this.bunny.scale.set(2.5);
-    this.bunny.anchor.set(0.5);
-    this.bunny.eventMode = "static";
-    this.bunny.cursor = "pointer";
-    this.app.stage.addChild(this.bunny);
+        this.bunny = new PIXI.Sprite(assets.bunny);
+        this.bunny.x = this.app.screen.width / 2;
+        this.bunny.y = this.app.screen.height / 2.5;
+        this.bunny.scale.set(2.5);
+        this.bunny.anchor.set(0.5);
+        this.bunny.eventMode = "static";
+        this.bunny.cursor = "pointer";
+        this.app.stage.addChild(this.bunny);
 
-    this.ui = new UI();
-    this.app.stage.addChild(this.ui);
+        this.ui = new UI();
+        this.app.stage.addChild(this.ui);
 
-    this.bunny.on("pointerdown", () => {
-      this.ui.addScore();
-    })
+        this.bunny.on("pointerdown", () => {
+            this.ui.addScore();
+        })
 
-    this.app.ticker.add(this.update.bind(this));
-  }
+        this.app.ticker.add(this.update.bind(this));
+    }
 
-  update(delta) {
-    this.bunny.rotation += 0.1 * delta;
-  }
+    update(delta) {
+        this.bunny.rotation += 0.1 * delta;
+    }
 }
